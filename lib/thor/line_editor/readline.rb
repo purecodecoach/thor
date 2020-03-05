purecodecoach/thor

commit 7061b06fd87dc8b952f9072507ee991ee0a0858e

Lazily require `readline`
The current implementation of readline has issues under Windows, and
those issues prevent thor from being used. For example, in our bundler
Windows CI, we had to patch `rb-readline` to workaround some issues
because `rb-readline` (the default `readline` provider on Windows)
couldn't be required. Moreover, we still get some issues when requiring
`rb-readline` like

```
HOME environment variable (or HOMEDRIVE and HOMEPATH) must be set and point to a directory
```

Since in some situations the part of thor using `readline`
(`Thor::LineEditor`) is not required, for example, in most of CI
situations. I thought it could be a good idea to lazily require
`readline`.

lib/thor/line_editor/readline.rb
@@ -1,12 +1,12 @@
-begin
-  require "readline"
-rescue LoadError
-end
-
 class Thor
   module LineEditor
     class Readline < Basic
       def self.available?
+        begin
+          require "readline"
+        rescue LoadError
+        end
+
         Object.const_defined?(:Readline)
       end
 

commit 40838045333348df12a9c1c13b9956b78d863179

Fix readline specs
An error was introduced in commit 7061b06fd87dc8b952f9072507ee991ee0a0858e
where readline specs fail because `require "readline"` is be called after the
spec has established a mock `::Readline`. Prior to that change
`require "readline"` was always only called once.

This patch:
  * memoizes the `require` to ensure that it only happens once
  * leaves the `Object.const_defined?` check in place to keep tests simple
  * eagerly calls `available?` in the readline spec to match the original
    author's intent by requiring `readline` before the specs start.

lib/thor/line_editor/readline.rb
@@ -2,8 +2,8 @@ class Thor
   module LineEditor
     class Readline < Basic
       def self.available?
-        begin
-          require "readline"
+        @readline_reqd ||= begin
+            require "readline"
         rescue LoadError
         end
 

spec/line_editor/readline_spec.rb
@@ -2,6 +2,8 @@ require "helper"
 
 describe Thor::LineEditor::Readline do
   before do
+    # Eagerly check Readline availability and cache for this spec
+    Thor::LineEditor::Readline.available?
     unless defined? ::Readline
       ::Readline = double("Readline")
       allow(::Readline).to receive(:completion_append_character=).with(nil)